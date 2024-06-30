e2d7bab3037b34f96c5ec894635e293cf51fbca2
Kübay Telçeken <kubaytelceken@outlook.com>
2024-06-30 11:03:09 +0300

    Create Order Api & CQRS & Onion

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/DeleteAddressCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+    public class DeleteAddressCommand
+    {
+        public int AddressId { get; set; }
+
+        public DeleteAddressCommand(int addressId)
+        {
+            AddressId = addressId;
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/DeleteAddressCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class DeleteAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public DeleteAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(DeleteAddressCommand deleteAddressCommand)
+        {
+            var value = await _repository.GetByIdAsync(deleteAddressCommand.AddressId);
+            await _repository.DeleteAsync(value);
+
+        }
+    }
+}
