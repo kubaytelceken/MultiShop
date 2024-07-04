dbef1f19dbc78519be95692302b18e9e8e9d3842
Kübay Telçeken <kubaytelceken@outlook.com>
2024-07-04 23:35:13 +0300

    Mediator Design

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> OrderingById(int id)
+        {
+            var values = await _mediator.Send(new GetOrderingByIdQuery(id));
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering(CreateOrderingCommand createOrderingCommand)
+        {
+            await _mediator.Send(createOrderingCommand);
+            return Ok("Ordering added succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand updateOrderingCommand)
+        {
+            await _mediator.Send(updateOrderingCommand);
+            return Ok("Ordering updated succesfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOrdering(int id)
+        {
+            await _mediator.Send(new DeleteOrderingCommand(id));
+            return Ok("Ordering deleted successfully");
+        }
+    }
+}
--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,4 +1,27 @@
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+#region
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<DeleteAddressCommandHandler>();
+
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<DeleteOrderDetailCommandHandler>();
+#endregion
+
 
 // Add services to the container.
 
